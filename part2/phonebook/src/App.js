9611fcfb2e28889746023969c02e622439fc80da
Hien <hiennguyen18990@gmail.com>
2023-05-23 12:36:25 +0300

    delete token in test request

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,8 +5,7 @@ import PersonForm from './components/PersonForm';
 import Name from  './components/Persons';
 import Notification from './components/Notification';
 import personService from './services/person';
-
-
+import loginService from './services/login'
 
 
 const App = () => {
@@ -17,7 +16,11 @@ const App = () => {
   const [newSearch, setNewSearch] = useState('')
   const [alertMessage, setAlertMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
+  
   const hook = () => {
     personService
     .getAll()
@@ -26,6 +29,15 @@ const App = () => {
     })
   }
   useEffect(hook, [])
+
+  useEffect(() => {
+    const loggedUserJSOn = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSOn){
+      const loggedUser = JSON.parse(loggedUserJSOn)
+      setUser(loggedUser)
+      personService.setToken(loggedUser.token)
+    }
+  }, [])
 
   const updatePerson = ({newName, newNumber}) => {
     const thePerson = persons.find(p => p.name === newName)
@@ -43,6 +55,7 @@ const App = () => {
       setPersons(persons.filter(p => p.id !== id))
     })
   }
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
@@ -100,11 +113,49 @@ const App = () => {
   ? persons
   : persons.filter(person => person.name.toLowerCase().includes(newSearch))
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Notification message={alertMessage} nameClass="success" />
-      <Notification message={errorMessage} nameClass="error" />
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedInUser = await loginService.login({username, password})
+      window.localStorage.setItem('loggedUser', JSON.stringify(loggedInUser))
+      personService.setToken(loggedInUser.token)
+      setUser(loggedInUser)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+  )
+
+  const phonebookForm = () => (
+    <React.Fragment>
       <Filter search={newSearch} handleSearch={handleNewSearch} />
       <h2>Add new contact</h2>
       <PersonForm 
@@ -119,6 +170,22 @@ const App = () => {
         person => 
         <Name key={person.id} person={person} deleteClick={()=>deletePerson(person.id)}/>
         )}
+    </React.Fragment>
+  )
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={alertMessage} nameClass="success" />
+      <Notification message={errorMessage} nameClass="error" />
+
+      {!user && loginForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+        {phonebookForm()}
+        </div>
+      }
+      
+      
     </div>
   )
 }
--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
-
 const baseUrl = "/api/persons"
 
+let token = null
+
+const setToken = newToken => {
+    token = `Bearer ${newToken}`
+}
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => {
@@ -9,9 +13,12 @@ const getAll = () => {
     })
 }
 
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => { return response.data})
+const create = async newObject => {
+    const config = {
+        headers: {Authorization: token}
+    }
+    const response = axios.post(baseUrl, newObject, config)
+    return response.then(response => { return response.data})
 }
 
 const update = ({id, changedPerson}) => {
@@ -25,4 +32,4 @@ const deleteID = (id) =>{
             // return response.data
         
 } 
-export default {getAll, create, deleteID, update};
+export default {getAll, create, deleteID, update, setToken};
